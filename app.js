2a6c1085f983d1a8ae65b3bf7d3dbeb3ec92a49f
KT_Zheng <a6163508@gmail.com>
2019-04-02 16:09:09 +0800

    修改為 ES6 module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//import {render} from './helper';
+import {render} from './helper.js';
 
 const url = 'https://api.twitch.tv/kraken/streams/';
 const clientId = 'a7wvr2seuhub9br5h63epquw6c031h';
@@ -10,40 +10,6 @@ const radioNumber = document.getElementsByClassName("number");
 
 const container = document.getElementsByClassName("films-container")[0];
 
-function render(streams) {
-  streams.forEach(stream => {
-    console.log(stream);
-    let sectionFilm = document.createElement("section");
-    sectionFilm.className = "film";
-
-    let preview = stream.preview.medium;
-    let imgPreview = document.createElement("img");
-    imgPreview.className = "film-img";
-    imgPreview.src = preview;
-    sectionFilm.appendChild(imgPreview);
-
-    let channel = stream.channel;
-    let logo = channel.logo;
-    let name = channel.name;
-    //let link = channel.url;
-
-    let divData = document.createElement("div");
-    divData.className = "film-data";
-    let imgLogo = document.createElement("img");
-    imgLogo.className = "data-img";
-    imgLogo.src = logo;
-    let divName = document.createElement("div");
-    divName.className = "data-info";
-    divName.textContent = name;
-
-    divData.appendChild(imgLogo);
-    divData.appendChild(divName);
-    sectionFilm.appendChild(divData);
-
-    container.appendChild(sectionFilm);
-  });
-}
-
 const getTwitch = async () => {
   const endpoint = `${url}?client_id=${clientId}&game=${game}&limit=${limit}`;
 
@@ -51,7 +17,7 @@ const getTwitch = async () => {
     const response = await fetch(endpoint);
     if (response.ok) {
       const jsonResponse = await response.json();
-      render(jsonResponse.streams);
+      render(jsonResponse.streams, container);
     } else {
       throw new Error('Oh my god!');
     }
--- helper.js
+++ helper.js
@@ -1,3 +1,37 @@
-export default function (streams) {
+export function render(streams, container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
+  streams.forEach(stream => {
+    let sectionFilm = document.createElement("section");
+    sectionFilm.className = "film";
+
+    let preview = stream.preview.medium;
+    let imgPreview = document.createElement("img");
+    imgPreview.className = "film-img";
+    imgPreview.src = preview;
+    sectionFilm.appendChild(imgPreview);
+
+    let channel = stream.channel;
+    let logo = channel.logo;
+    let name = channel.name;
+    let link = channel.url;
+
+    let divData = document.createElement("div");
+    divData.className = "film-data";
+    let imgLogo = document.createElement("img");
+    imgLogo.className = "data-img";
+    imgLogo.src = logo;
+    let aName = document.createElement("a");
+    aName.className = "data-info";
+    aName.textContent = name;
+    aName.href = link;
+
+    divData.appendChild(imgLogo);
+    divData.appendChild(aName);
+    sectionFilm.appendChild(divData);
+
+    container.appendChild(sectionFilm);
+  });
 }
